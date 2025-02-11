86cdff8d6197aabc469c8f2ff4a426d4e35703ea
SantiGomez2519 <sgomezo14@eafit.edu.co>
2025-02-10 23:30:17 -0500

    Fix and Add - File handling and destination as first app

--- AntioquiaOffTheMap/urls.py
+++ AntioquiaOffTheMap/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+# The `urlpatterns` list routes URLs to views.
+from destination import views as destination_views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', destination_views.home, name='home'),
 ]
