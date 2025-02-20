cb91651c0330551ffa0e1f80f5329cb8a0e27dc7
SantiGomez2519 <sgomezo14@eafit.edu.co>
2025-02-20 01:48:39 -0500

    Update - Landing page styling updated

--- landing/views.py
+++ landing/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from destination.models import Municipality, Event
 
 # Create your views here.
 
 def landing(request):
-    return render(request, 'landing.html')
+    municipalities = Municipality.objects.all()
+    return render(request, 'landing.html', {'municipalities': municipalities})
