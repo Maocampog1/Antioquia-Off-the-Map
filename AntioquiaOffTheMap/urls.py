d552e2cd9d128fbcd794470c846130ae2ef161c6
SantiGomez2519 <sgomezo14@eafit.edu.co>
2025-04-10 00:49:59 -0500

    Feature - Login template V1

--- AntioquiaOffTheMap/urls.py
+++ AntioquiaOffTheMap/urls.py
@@ -23,6 +23,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("destination.urls")),
+    path("", include("user.urls")),
 
 
     path("__reload__/", include("django_browser_reload.urls")),
