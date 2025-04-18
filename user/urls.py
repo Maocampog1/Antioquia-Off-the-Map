32e18fb2c98de5650d7bd40bdcaf06f4cc383468
SantiGomez2519 <sgomezo14@eafit.edu.co>
2025-04-18 17:19:07 -0500

    Feature - Login Santi

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
+    path('registro/', views.register, name='register'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,26 +1,43 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth import logout as django_logout
 from django.contrib import messages
+from .forms import CustomUserCreationForm
 
-# Create your views here.
-def login(request):
-    if request.user.is_authenticated:
-        return redirect('home')  # Si ya está autenticado, lo redirige a la página principal
-    
+def login_user(request):
     if request.method == "POST":
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        
+        username = request.POST['username']
+        password = request.POST['password']
         user = authenticate(request, username=username, password=password)
-        
         if user is not None:
             login(request, user)
-            return redirect('home')  # Redirigir a la página principal
+            messages.success(request, "Has iniciado sesión correctamente.")
+            return redirect('home')
         else:
-            messages.error(request, "Usuario o contraseña incorrectos")
+            
+            messages.error(request, "Hubo un error al iniciar sesión, intenta de nuevo.")  
+            return redirect('login')    
+    else:
+        return render(request, "login.html")
 
-    return render(request, 'login.html')
+def logout_user(request):
+    django_logout(request)
+    messages.success(request, "¡Cerraste sesión correctamente!")
+    return redirect('home')
 
-def logout(request):
-    logout(request)
-    return redirect('home')  # Redirigir a la página principal después de cerrar sesión
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "¡Te has registrado exitosamente!")
+            return redirect('home')
+        else:
+            # if the form is not valid, show the errors to the user
+            messages.warning(request, "Por favor corrige los errores en el formulario.")
+    else:
+        form = CustomUserCreationForm()
+    
+    return render(request, 'register.html', {'form': form})
